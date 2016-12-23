7a762260d2e5f3d2f36d1e27b74e1506fc34c28a
yankee <yankee@debian>
2016-12-23 23:09:22 +0545

    Strong Parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :photo, PhotoUploader 
+  has_many :posts
 end
