39dd54f7cf2b7447736ac1427bc273b13fb2ed56
yankee <yankee@debian>
2016-12-24 19:44:51 +0545

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,42 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
+	 def index 
+  		@posts = Post.all.order('created_at DESC')
+  	end
+
 	def new 
 		@post = Post.new
 	end 
 
 	def create
 		@post=current_user.posts.create(post_params)
-		if @post.valid?
+		if @post.valid?	
 			redirect_to root_path
 		else
 			render :new, status: :unprocessable_entity
 		end
 	end
  
+ 	def edit 
+ 		@post = Post.find(params[:id])
+ 	end
+
+ 	def update 
+ 		@post = Post.find(params[:id])
+ 		@post.update(post_params)
+ 		if @post.valid?
+ 			redirect_to root_path
+ 		else 
+ 			render :edit, status: :unprocessable_entity
+ 		end
+ 	end
+
+ 	
  private
 
  def post_params
-  	params.require(:post).permit(user_id, :photo, :description)
+  	params.require(:post).permit(:user_id, :photo, :description)
   end
   
-  def index 
-  	@posts = Post.all.order('created_at DESC')
-  end
+ 
  end 
