6a283ad25f7b7b218c79945c41eb4094b5a6e3b6
yankee <yankee@debian>
2016-12-29 09:11:52 +0545

    Navbar added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!, only: :create
+	before_action :is_owner?, only: :destroy
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
@@ -11,10 +12,25 @@ class CommentsController < ApplicationController
 			end
 	end
 
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
+		redirect_to root_path
+	end
+
+
+
+
 	private 
 	def comment_params
 		params.require(:comment).permit(:text, :post_id)
 	end
 
-
+	def is_owner?
+		@comment = Comment.find(params[:id])
+		if @comment.user != current_user
+			redirect_to root_path
+		end
+	end
+	
 end
